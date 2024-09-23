be85fac88a4626675256da249033fdbf29a455d8
DavilGM <127234685+DavilGM@users.noreply.github.com>
2024-09-24 03:00:18 +0700

    1st part completed - backend

---
+++ backend/controller/user.controller.js
@@ -0,0 +1,12 @@
+import User from "../model/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filtereUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+        res.status(200).json(filtereUsers);
+    } catch (error) {
+        console.error("Error in getUsersForSidebar", error.message)
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
